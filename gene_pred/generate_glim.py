a8d6705c63c6ddac0d2955e2e2be467fe3075c59
Sachin Kumar <skumar435@gatech.edu>
2019-03-01 03:52:09 -0500

    bug fixes for gff conversion

--- gene_pred/generate_glim.py
+++ gene_pred/generate_glim.py
@@ -4,28 +4,30 @@ import re
 import subprocess
 
 def generateGlimOP(argDir):
-	lastFileIndex = 0
-	for (dirpath, dirnames, filenames) in os.walk(argDir):
-		if not filenames:
-			print ('Empty directory!')
-		for file in filenames:
-			run = "run"+str(filenames.index(file))
-			subprocess.run(["glimmer3.02/scripts/g3-from-scratch.csh", argDir+file, run])
-			lastFileIndex = str(filenames.index(file))
+	contigFileNames = []
+	for file in os.listdir(argDir):
+		if os.path.isfile(argDir + file):
+			contigFN = file.split(".")[0]
+			subprocess.run(["glimmer3.02/scripts/g3-from-scratch.csh", argDir + file, contigFN])
+			contigFileNames.append("temp/" + contigFN + ".predict")
 	subprocess.run(["mkdir", "-p", "./temp/"])
-	supbprocess.run(["mv", "run*", "./temp/"])
-	return (lastFileIndex+1)
+	for file in contigFileNames:
+		subprocess.run(["mv", file, "./temp/"])
+	return contigFileNames
 
 def convertToGFF(content):
 	header = ""
 	formatted_content = []
 	for line in content:
 		if line.startswith(">"):
-			header = line.split("|")[2].split()[0]
-			formatted_content.append(line.replace(">","#"))
+			header = line[1:].rstrip()
+			formatted_content.append(line.replace(">", "#"))
 		else:
 			line_format = line.split()
-			formatted_content.append("{}\tGLIMMER\tgene\t{}\t{}\t{}\t{}\t{}\tID={}\n".format(header,line_format[1],line_format[2],line_format[4],line_format[3][0],line_format[3][1:],line_format[0]))
+			if int(line_format[2]) < int(line_format[1]):
+				formatted_content.append("{}\tGLIMMER\tgene\t{}\t{}\t{}\t{}\t{}\tID={}\n".format(header,line_format[2],line_format[1],line_format[4],line_format[3][0],line_format[3][1:],line_format[0]))
+			else:
+				formatted_content.append("{}\tGLIMMER\tgene\t{}\t{}\t{}\t{}\t{}\tID={}\n".format(header,line_format[1],line_format[2],line_format[4],line_format[3][0],line_format[3][1:],line_format[0]))
 	return formatted_content
 
 def readWriteFile(fileName):
@@ -36,17 +38,13 @@ def readWriteFile(fileName):
 		with open(writeFName, 'w') as fwh:
 			fwh.writelines(new_content)
 
-def convertAllToGFF(tempDir, contNum):
-	for (dirpath, dirnames, filenames) in os.walk(tempDir):
-		if not filenames:
-			print ('Empty directory!')
-		for fileNum in range(contNum):
-			fileN = "{}run{}.predict".format(tempDir, fileNum)
-			readWriteFile(fileN)
+def convertAllToGFF(contNames):
+	for file in contNames:
+		readWriteFile(file)
 
 def main():
 	argDir = sys.argv[1]
-	num_files = generateGlimOP(argDir)
-	convertAllToGFF("./temp/", num_files)
+	glim_files = generateGlimOP(argDir)
+	convertAllToGFF(glim_files)
 
 main()
