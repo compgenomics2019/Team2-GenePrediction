bb06f968d5813bfe60e259c74fe44b89626dc7bb
Sachin Kumar <skumar435@gatech.edu>
2019-03-07 04:45:01 -0500

    comments updated

--- gene_pred/gene_prediction.py
+++ gene_pred/gene_prediction.py
@@ -9,6 +9,7 @@ from scripts.prodigal_script import prodigal
 from scripts.genemarkS2 import genemarkS2
 from scripts.RNAsearch import RNApredict
 
+# creates the output directories for the output of prodigal
 def createProdOutDirs():
 	if not os.path.exists(os.getcwd()+"/output/"):
 		os.makedirs(os.getcwd()+"/output")
@@ -19,6 +20,7 @@ def createProdOutDirs():
 	if not os.path.exists(os.getcwd()+"/output/out_prod/nucl"):
 		os.makedirs(os.getcwd()+"/output/out_prod/nucl")
 
+# creates the op dirs for output of gene mark s2
 def createGMOutDirs():
 	if not os.path.exists(os.getcwd()+"/output/out_gms2/"):
 		os.makedirs(os.getcwd()+"/output/out_gms2")
@@ -27,6 +29,7 @@ def createGMOutDirs():
 	if not os.path.exists(os.getcwd()+"/output/out_gms2/nucl"):
 		os.makedirs(os.getcwd()+"/output/out_gms2/nucl")
 
+# get the intersection genes from prodigal output
 def getProdIntGene(argDir):
 	gm_out_dir = "./output/out_gms2/"
 	prod_out_dir = "./output/out_prod/"
@@ -37,6 +40,7 @@ def getProdIntGene(argDir):
 		subprocess.run(['bedtools', 'intersect', '-f', '1.0', '-r', '-v', '-wa', '-a', prod_out_dir+fName, '-b', gm_out_dir+fName, '>', prod_out_dir+nFName])
 		subprocess.run(['bedtools', 'intersect', '-f', '1.0', '-r', '-wa', '-a', prod_out_dir+fName, '-b', gm_out_dir+fName, '>', prod_out_dir+oFName])
 
+# get intersection genes from genemark op
 def getGMIntGene(argDir):
 	gm_out_dir = "./output/out_gms2/"
 	prod_out_dir = "./output/out_prod/"
@@ -45,6 +49,7 @@ def getGMIntGene(argDir):
 		nFName = file.split(".")[0] + "_int" + ".gff"
 		subprocess.run(['bedtools', 'intersect', '-f', '1.0', '-r', '-v', '-wa', '-a', gm_out_dir+fName, '-b', prod_out_dir+fName, '>', gm_out_dir+nFName])
 
+# gets the union of the genes from prodigal and gene mark
 def mergeGFF(argDir):
 	out_dir = "./output/"
 	gm_out_dir = "./output/out_gms2/"
@@ -56,6 +61,7 @@ def mergeGFF(argDir):
 		fName3 = prod_out_dir + file.split(".")[0] + "_int2" + ".gff"
 		subprocess.run(['cat', fName1, fName2, fName3, '>', fName])
 
+# merges the genes with the rna content generated
 def mergeRNAGFF(argDir):
 	out_dir = "./output/"
 	out_dir_RNA = "./output/out_rna/"
@@ -71,8 +77,9 @@ def main():
 	parser.add_argument('-f', '--format', help='Output Format (gff, gbk, sqn, sco)', default='gff', type=str)
 	parser.add_argument('-g', '--genemark', help='Optional arg for using genemark', action='store_true')
 	args = parser.parse_args()
-	
+	# output dir for the rna results
 	out_dir_RNA = "./output/out_rna/"
+	# if gene mark need to be run on the contigs
 	if args.genemark:
 		createGMOutDirs()
 		createProdOutDirs()
@@ -85,7 +92,6 @@ def main():
 		getProdIntGene(args.input)
 		getGMIntGene(args.input)
 		mergeGFF(args.input)
-
 	else:
 		createProdOutDirs()
 		prod_out_dir = "./output/out_prod/"
